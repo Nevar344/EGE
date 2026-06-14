89d32678a17b1fc7c4460cdd5915b48e11060a20
D&Arther <69269473+Nevar344@users.noreply.github.com>
2026-06-14 19:45:04 +0300

    Update 23teap.py

--- 23teap.py
+++ 23teap.py
@@ -108,3 +108,30 @@
 #         s += F(new_x, y)
 #     return s
 # print(F(110, 154))
+
+# def F(x, y):
+#     if x > y:
+#         return 0
+#
+#     if x == y:
+#         return 1
+#
+#     if x < y:
+#         return F(x + 1, y) + F(x + 2, y) + F(x * 3, y)
+#
+# print(F(2, 9) * F(9, 11) * F(11, 13))
+# 
+# Ответ: 100
+
+# def F(x, y):
+#     if x > y or x == 15:
+#         return 0
+#
+#     if x == y:
+#         return 1
+#
+#     if x < y:
+#         return F(x + 1, y) + F(x * 2, y) + F(x * 3, y)
+#
+# print(F(1, 11) * F(11, 25))
+
