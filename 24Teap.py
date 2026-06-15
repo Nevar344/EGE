f13d11464c576c732dc948e3aed5a482027efc99
D&Arther <69269473+Nevar344@users.noreply.github.com>
2026-06-15 19:55:47 +0300

    Update 24Teap.py

--- 24Teap.py
+++ 24Teap.py
@@ -130,3 +130,152 @@
 #             mx = max(len(s), mx)
 #
 # print(mx)
+
+# Найдите максимальную длину подстроки, в которой символы a и d не стоят рядом
+# a = open('24').readline()
+# a = a.replace('ad', ' a d').replace('da', 'd a').split()
+# mx = 0
+# for s in a:
+#     mx = max(mx, len(s))
+# print(mx)
+
+#Определите максимальное количество идущих подряд пар символов вида согласная + гласная
+#A, B, C, D и O.
+# a = open('24-2.txt').readline()
+# for i in 'BCD': a = a.replace(i, 'B')
+# for i in 'AO': a = a.replace(i, 'A')
+# a = a.replace('BA', '*')
+# a = a.replace('A', ' ').replace('B', ' ').split()
+# mx = 0
+# for s in a:
+#     mx = max(mx, len(s))
+# print(mx)
+
+#Регулярки
+#Определите максимальное количество подряд идущих последовательностей символов NPO или PNO в прилагаемом файл
+#последовательность должна состоять только из троек NPO, или только из троек PNO, или только из троек NPO и PNO в произвольном порядке их следования.
+# from re import *
+# a = open('24-4.txt').readline()
+# case = r'((NPO)|(PNO))+'
+# mx = 0
+# for i in finditer(case, a):
+#     mx = max(len(i.group(0)), mx)
+# print(mx // 3)
+
+#Циклами Текстовый файл состоит из символов T, U, V, W, X, Y и Z
+#Определите в прилагаемом файле максимальное количество идущих подряд символов (длину
+# непрерывной подпоследовательности), среди которых символ Y встречается не более 150
+# a = open('24-4.txt').readline()
+# mx = 1
+# for l in range((len(a))):
+#     for r in range(l+mx, len(a)):
+#         s = a[l: r+1]
+#         if s.count('Y') > 150: break
+#         if s.count('Y') <= 150: mx = max(mx, len(s))
+# print(mx)
+
+#файл состоит из символов, обозначающих заглавные буквы латинского алфавита и цифры от 1 до 9 включительно
+#которые могут представлять запись числа в шестнадцатеричной
+#Цифры, числовое значение которых превышает 9, обозначены латинскими
+# from re import *
+# a = open('24-5.txt').readline()
+# case = r'[1-F][0-F]*'
+# mx = 0
+# for i in finditer(case, a):
+#     mx = max(mx, len(i.group(0)))
+# print(mx)
+
+#файл состоит из символов T, U, V, W, X, Y и Z.
+#среди которых символ T встречается ровно 100 раз
+# a = open('24-6.txt').readline()
+# mx = 0
+# a = a.split('T')
+# for i in range(len(a)-100):
+#     s = 'T'.join(a[i:i+101])
+#     mx = max(mx, len(s))
+# print(mx)
+
+#Текстовый файл состоит из символов A, B, C, D, E и F
+#которых пара символов CD (в указанном порядке) встречается ровно 160 раз
+# a = open('24-7.txt').readline()
+# mx = 0
+# a = a.replace('CD', 'C D').split()
+# for i in range(len(a)-160):
+#     s = ''.join(a[i:i+161])
+#     mx = max(mx, len(s))
+# print(mx)
+
+#подряд символов, среди которых ровно 35 букв S, начинающуюся чётной цифрой не содержащую других чётных цифр, кроме первой
+# a = open('24-8.txt').readline()
+# mx = 0
+# for i in '02468': a = a.replace(i, '0')
+# a = a.split('0')
+# for s in a:
+#     while s.count('S') >= 35:
+#         if s.count('S') == 35: mx = max(mx, len(s))
+#         s = s[:-1]
+# print(mx)
+
+# среди которых ровно 35 букв Q, начинающуюся нечётной цифрой не содержащую других нечётных цифр, кроме первой
+# a = open('24-9.txt').readline()
+# mx = 1
+# for i in '13579': a = a.replace(i, '1')
+# for l in range(len(a)):
+#     if a[l] in '1':
+#         for r in range(l + mx, len(a)):
+#             s = a[l:r+1]
+#             if s.count('1') > 1 or s.count("Q") > 35: break
+#             if s.count('1') == 1 and s.count("Q") == 35:
+#                 mx = max(mx, len(s))
+# print(mx)
+
+#которых подстрока 2025 встречается не менее 90 раз и при этом содержится ровно 80 букв Y.
+# a = open('24-10.txt').readline()
+# mx = 1
+# for l in range(len(a)):
+#     for r in range(l + mx, len(a)):
+#         s = a[l:r+1]
+#         if s.count('Y') > 80: break
+#         if s.count('2025') >= 90 and s.count('Y') == 80:
+#             mx = max(mx, len(s))
+# print(mx)
+
+#подряд одинаковых букв, начинающуюся и заканчивающуюся чётной цифрой, не содержащую
+#других букв, кроме повторяющихся, не содержащую цифр, кроме первой и последней
+# a = open('24-11.txt').readline()
+# for i in '02468': a = a.replace(i, '0')
+#
+# a = a.split('0')
+# mx = 0
+# for s in a:
+#     if len(set(s)) == 1 and s[0] not in '13579':
+#         mx = max(mx, len(s) + 2)
+# print(mx)
+
+#начинается и заканчивается одинаковой гласной буквой и не содержит букв внутри
+# from re import *
+# a = open('24-12.txt').readline()
+# mx = 0
+# for b in 'AEIOUY':
+#     case = rf'[{b}][^A-Z]+[{b}]'
+#     for i in finditer(case, a):
+#         mx = max(len(i.group(0)), mx)
+# print(mx)
+
+
+# a = open('24-12.txt').readline()
+# for i in '0123456789': a = a.replace(i, '0')
+# mx = 0
+#
+# for l in range(len(a)):
+#     if a[l] in 'AEIOUY':
+#         c = 1
+#         for r in range(l + 1, len(a)):
+#             if a[r] == '0':
+#                 c += 1
+#             if a[r] != '0':
+#                 if a[r] == a[l]:
+#                     c += 1
+#                     mx = max(mx, c)
+#                 break
+# print(mx)
